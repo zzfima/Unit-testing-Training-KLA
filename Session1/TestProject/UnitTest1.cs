98a4e252c95a24b83a25f8cceb6794b0f255d569
zzfima <zzfima@gmail.com>
2022-09-04 22:30:43 +0300

    test perist and read

--- Session1/TestProject/UnitTest1.cs
+++ Session1/TestProject/UnitTest1.cs
@@ -16,5 +16,17 @@ namespace TestProject
             File.Exists(file.FullName).Should().BeTrue();
             File.Delete(file.FullName);
         }
+
+        [TestMethod]
+        public void ReservationShouldPersistAndRead()
+        {
+            var reservationPersister = new ReservationCrud();
+            var random = new Random();
+            var reservation = new Reservation(random.Next(), "Alex", 5);
+            var file = reservationPersister.Persist(reservation);
+            var readReservation = reservationPersister.Read(file);
+            reservation.Should().BeEquivalentTo(readReservation);
+            File.Delete(file.FullName);
+        }
     }
 }
