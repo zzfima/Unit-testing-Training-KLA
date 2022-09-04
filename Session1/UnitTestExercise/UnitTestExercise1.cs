8c0f753dbf27882a91cb60ccaebfd7e803c692aa
zzfima <zzfima@gmail.com>
2022-09-04 21:34:03 +0300

    Start

---
+++ Session1/UnitTestExercise/UnitTestExercise1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace UnitTestExercise
+{
+    public interface IConnectivityReporter
+    {
+        void AttemptFailed(string ip);
+        void ConnectionFailed(string ip);
+    }
+
+    public interface IPing : IDisposable
+    {
+        PingReply Send(string ip, int millisec);
+    }
+
+    public interface IPingFactory
+    {
+        IPing Create();
+    }
+
+    class Monitor
+    {
+        private readonly IConnectivityReporter connectivityReporter;
+        private readonly IPingFactory pingFactory;
+
+        public Monitor(IConnectivityReporter connectivityReporter, IPingFactory pingFactory)
+        {
+            this.connectivityReporter = connectivityReporter;
+            this.pingFactory = pingFactory;
+        }
+
+        public void CheckComputerAvailability(string ip, CancellationToken cancellationToken, TimeSpan timeout)
+        {
+            Stopwatch timeUsed = Stopwatch.StartNew();
+            int millisecsLeft = (int)timeout.TotalMilliseconds;
+
+            using (var ping = pingFactory.Create())
+            {
+                while (millisecsLeft > 0 && !cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        PingReply reply = ping.Send(ip, millisecsLeft);
+                        if (reply?.Status == IPStatus.Success)
+                        { 
+                            return; 
+                        }
+
+                        connectivityReporter.AttemptFailed(ip);
+                    }
+                    catch
+                    {
+                    }
+                    millisecsLeft = (int)(timeout - timeUsed.Elapsed).TotalMilliseconds;
+                }
+            }
+            connectivityReporter.ConnectionFailed(ip);
+        }
+    }
+}
---
+++ Session1/UnitTestExercise/UnitTestExercise2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace UnitTestExercise
+{
+    public class Reservation
+    {
+        public Reservation(int id, string reserverName, int quantity)
+        {
+            Id = id;
+            ReserverName = reserverName;
+            Quantity = quantity;
+            PersisteReservation();
+        }    
+
+        public int Id { get; }
+
+        public string ReserverName { get; }
+
+        public int Quantity { get; }
+
+        private void PersisteReservation()
+        {
+            var serializer = new XmlSerializer(typeof(Reservation));
+            using (var stream = new FileStream("Reservation " + Id, FileMode.CreateNew))
+            {               
+                serializer.Serialize(stream, this);
+            }
+        }
+    }
+
+    public class Restaurant
+    {
+        public Restaurant(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; }
+
+        public bool CanAccept(IEnumerable<Reservation> existingReeservations, Reservation newReservation)
+        {
+            var reservedSeats = existingReeservations.Sum(r => r.Quantity);
+
+            if (Capacity > reservedSeats + newReservation.Quantity)
+                return true;
+
+            return false;
+        }
+    }
+}
