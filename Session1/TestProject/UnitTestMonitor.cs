42aa1fd778c37fac9363e0d7e2e174eaea4fc357
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-09-05 07:36:21 +0300

    add test of bad connection

--- Session1/TestProject/UnitTestMonitor.cs
+++ Session1/TestProject/UnitTestMonitor.cs
@@ -8,48 +8,64 @@ namespace TestProject
     [TestClass]
     public class UnitTestMonitor
     {
-        private Mock<IConnectivityReporter>? _mockConnectivityReporter;
-        private Mock<IPingFactory>? _mockPingFactory;
+        private MonitorCustomCustom? _monitor;
+        private bool _isAttemptFailed;
+        private int _attemptFailedCounter;
+        private bool _isConnectionFailed;
+        private Mock<IPingReplyCustom> _mockPingReplyCustom;
 
         [TestInitialize]
         public void Initialize()
         {
             //Arrange
-            _mockConnectivityReporter = new Mock<IConnectivityReporter>();
-            _mockPingFactory = new Mock<IPingFactory>();
+            Mock<IConnectivityReporter> mockConnectivityReporter = new Mock<IConnectivityReporter>();
+            _mockPingReplyCustom = new Mock<IPingReplyCustom>();
+            Mock<IPing> mockPing = new Mock<IPing>();
+            Mock<IPingFactory> mockPingFactory = new Mock<IPingFactory>();
+            _isAttemptFailed = false;
+            _attemptFailedCounter = 0;
+            _isConnectionFailed = false;
+
+            mockConnectivityReporter.Setup(m => m.AttemptFailed(It.IsAny<string>())).Callback(() =>
+            {
+                _isAttemptFailed = true;
+                _attemptFailedCounter++;
+            });
+            mockConnectivityReporter.Setup(m => m.ConnectionFailed(It.IsAny<string>())).Callback(() => _isConnectionFailed = true);
+            _mockPingReplyCustom.Setup(m => m.Status).Returns(IPStatus.Success);
+            mockPing.Setup(m => m.Send(It.IsAny<string>(), It.IsAny<int>())).Returns(_mockPingReplyCustom.Object);
+            mockPingFactory.Setup(m => m.Create()).Returns(mockPing.Object);
+
+            _monitor = new MonitorCustomCustom(mockConnectivityReporter.Object, mockPingFactory.Object);
         }
 
         [TestMethod]
         public void ShouldComputerBeAvailable()
         {
-            //Arrange
-            Mock<IConnectivityReporter> mockConnectivityReporter = new Mock<IConnectivityReporter>();
-            Mock<IPingReplyCustom> mockPingReplyCustom = new Mock<IPingReplyCustom>();
-            Mock<IPing> mockPing = new Mock<IPing>();
-            Mock<IPingFactory> mockPingFactory = new Mock<IPingFactory>();
-            bool isAttemptFailed = false;
-            int attemptFailedCounter = 0;
-            bool isConnectionFailed = false;
-
-            mockConnectivityReporter.Setup(m => m.AttemptFailed(It.IsAny<string>())).Callback(() =>
-            {
-                isAttemptFailed = true;
-                attemptFailedCounter++;
-            });
-            mockConnectivityReporter.Setup(m => m.ConnectionFailed(It.IsAny<string>())).Callback(() => isConnectionFailed = true);
-            mockPingReplyCustom.Setup(m => m.Status).Returns(IPStatus.Success);
-            mockPing.Setup(m => m.Send(It.IsAny<string>(), It.IsAny<int>())).Returns(mockPingReplyCustom.Object);
-            mockPingFactory.Setup(m => m.Create()).Returns(mockPing.Object);
-
-            var monitor = new MonitorCustomCustom(mockConnectivityReporter.Object, mockPingFactory.Object);
+            //Arrange at initialize
 
             //Act
-            monitor.CheckComputerAvailability(It.IsAny<string>(), new CancellationToken(), new TimeSpan(0, 0, 1));
+            _monitor?.CheckComputerAvailability(It.IsAny<string>(), new CancellationToken(), new TimeSpan(0, 0, 1));
 
             //Assert
-            isAttemptFailed.Should().BeFalse();
-            isConnectionFailed.Should().BeFalse();
-            attemptFailedCounter.Should().Be(0);
+            _isAttemptFailed.Should().BeFalse();
+            _isConnectionFailed.Should().BeFalse();
+            _attemptFailedCounter.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void ShouldComputerUnaavailable()
+        {
+            //Arrange at initialize
+                        _mockPingReplyCustom.Setup(m => m.Status).Returns(IPStatus.Unknown);
+
+            //Act
+            _monitor?.CheckComputerAvailability(It.IsAny<string>(), new CancellationToken(), new TimeSpan(0, 0, 1));
+
+            //Assert
+            _isAttemptFailed.Should().BeTrue();
+            _isConnectionFailed.Should().BeTrue();
+            //_attemptFailedCounter.Should().Be(0);
         }
 
         [TestCleanup]
